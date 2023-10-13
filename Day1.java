8c925e15684d11972ee4a784d2e08cc1f70db74d
Nur Avika <146628831+nuravika031@users.noreply.github.com>
2023-10-13 11:58:53 +0800

    Menampilkan Hello World.java

---
+++ Day1.java
@@ -0,0 +1,8 @@
+package day1;
+public class Day1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, Nur Avika!");
+    }
+    
+}
