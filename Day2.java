6e6d206c44359d6ba65d4e8e8f6f48b28bb06ee0
Nur Avika <146628831+nuravika031@users.noreply.github.com>
2023-10-13 11:59:59 +0800

    Menampilkan Data diri.java

---
+++ Day2.java
@@ -0,0 +1,20 @@
+package day2;
+public class Day2 {
+    public static void main(String[] args) {
+        String nama = "Nur Avika";
+        String alamat = "Polewali Mandar";
+        char gender = 'P';
+        int umur = 18;
+        double tinggiBadan = 155.7;
+        float beratBadan = 45f;
+        boolean statusKesehatan = true;
+        System.out.println("Nama \t\t:" + nama);
+        System.out.println("Alamat \t\t:" + alamat);
+        System.out.println("Gender \t\t:" + gender);
+        System.out.println("Umur \t\t:" + umur + " tahun");
+        System.out.println("Tinggi Badan \t:" + tinggiBadan + " cm");
+        System.out.println("Berat Badan \t:" + beratBadan + " kg");
+        System.out.println("Sehat \t\t:" + statusKesehatan);
+    }
+    
+}
