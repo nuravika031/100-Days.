d9b2cbefe17b1f85aa823dd4d1ba57cbff0ea391
Nur Avika <146628831+nuravika031@users.noreply.github.com>
2023-10-17 20:31:38 +0800

    Type Casting

---
+++ Day9.java
@@ -0,0 +1,26 @@
+package day9;
+public class Day9 {
+    public static void main(String[] args) {
+        //short ke double
+        short data1 = 457;
+        double double_data = data1; 
+        System.out.println("Short ke Double: "+ double_data);
+        //char ke long
+        char data2 = 'A';
+        long long_data = data2;
+        System.out.println("Char ke Long: "+long_data);
+        // int ke float
+        int  data3 = 456;
+        float int_data = data3;
+        System.out.println("Int ke Float: "+int_data);
+        //byte ke long
+        byte data4 = 127;
+        long long_data2 = data4;
+        System.out.println("Byte ke Long: "+long_data2);
+        //float ke double
+        float data5 = 565.3f;
+        double double_data2 = data5;
+        System.out.println("Float ke Double: "+double_data2);
+    }
+    
+}
