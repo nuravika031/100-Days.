d1a70a7c7e6a9e89f6bc0fa7e3cce9c4e3063ce5
Nur Avika <146628831+nuravika031@users.noreply.github.com>
2023-10-18 19:37:34 +0800

    Variabel

---
+++ Day10.java
@@ -0,0 +1,29 @@
+package day10;
+public class Day10 {
+    public static void main(String[] args) {
+         //Deklarasi variabel
+       String nama;
+       int usia;
+       double baratBadan;
+       boolean seorangMahasiswa;
+       char gender;
+       
+       //Inisialisasi variabel
+       int umur;
+       umur = 18;
+       double tinggiBadan;
+       tinggiBadan = 156;
+        System.out.println("Umur : " + umur);
+        System.out.println("Tinggi Badan: " + tinggiBadan);
+        
+       //variable update
+       byte angka = 100;
+       angka %= 27;
+        System.out.println("Angka : " + angka);
+        
+       //final variable
+       final String pesan = "Haii!, Masih semangat kuliah?";
+        System.out.println(pesan);
+    }
+    
+}
