1f352698754601f5ce49eb11f378afaa83778582
Nur Avika <146628831+nuravika031@users.noreply.github.com>
2023-10-25 22:54:00 +0800

    Perulangan For biasa

---
+++ Day17.java
@@ -0,0 +1,8 @@
+package day17;
+public class Day17 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i++) {
+            System.out.println("Angka ke-" + i);
+        }
+    }
+}
