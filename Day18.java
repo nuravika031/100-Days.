6682eeaf4d60518675bdef879dffdd0803e05a9f
Nur Avika <146628831+nuravika031@users.noreply.github.com>
2023-10-26 22:13:39 +0800

    Perulangan For Down To (backward).java

--- Day18.java
+++ Day18.java
@@ -2,7 +2,7 @@ package day18;
 public class Day18 {
     public static void main(String[] args) {
         for(int mahasiswa = 10; mahasiswa >= 1; mahasiswa--) {
-            System.out.println("Mahasiswa ke-" + (mahasiswa) + ": Lulus Quis");
+            System.out.println("Mahasiswa ke-" + (mahasiswa) + " : Lulus Quis");
     }
     
   }
