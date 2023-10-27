e8df9873b5110e67d51156d4eb390ede1e3d7755
Nur Avika <146628831+nuravika031@users.noreply.github.com>
2023-10-27 12:19:16 +0800

    Perulangan For up to (fordward)

---
+++ Day19.java
@@ -0,0 +1,8 @@
+package day19;
+public class Day19 {
+    public static void main(String[] args) {
+       for(int hari = 1; hari <= 7; hari++) {
+           System.out.println("Hari ke-" + (hari) + ": ");
+    }
+  }  
+}
