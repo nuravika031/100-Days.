fe2453bc68e1545a67a824dc701c1c1ec83d6d3d
Nur Avika <146628831+nuravika031@users.noreply.github.com>
2023-10-29 09:35:52 +0800

    Perulangan For Increment

---
+++ Day21.java
@@ -0,0 +1,10 @@
+package day21;
+public class Day21 {
+    public static void main(String[] args) {
+      //Mencuci pakaian setiap dua hari sekali
+         for (int hari = 1; hari <= 30; hari += 2) {
+             System.out.println("Hari ke-" + hari + ": Cuci pakaian");
+          }
+        }
+    }
+   
