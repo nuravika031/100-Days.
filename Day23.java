082d0fd350bd6c29365dea4a9b3253da7cf8b235
Nur Avika <146628831+nuravika031@users.noreply.github.com>
2023-10-31 17:10:06 +0800

    Perulangan while

---
+++ Day23.java
@@ -0,0 +1,11 @@
+package day23;
+public class Day23 {
+    public static void main(String[] args) {
+        int bilanganGanjil = 1;
+        while(bilanganGanjil <= 30) {
+            System.out.println(bilanganGanjil);
+            bilanganGanjil+=2;
+    }
+    
+  }
+}    
