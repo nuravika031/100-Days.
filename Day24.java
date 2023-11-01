af5429ec5f9b711b91142c0a7753bc4133115111
Nur Avika <146628831+nuravika031@users.noreply.github.com>
2023-11-01 17:34:55 +0800

    Perulangan Do while

---
+++ Day24.java
@@ -0,0 +1,12 @@
+package day24;
+public class Day24 {
+    public static void main(String[] args) {
+        int genap = 2;
+        
+        do {
+            System.out.println("Nilai genap: " + genap);
+            genap +=2;
+        } while (genap <= 11);
+    }
+    
+}
