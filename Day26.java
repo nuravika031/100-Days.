1b297e8c853b2582b2e409c5c7db146ccb4b225b
Nur Avika <146628831+nuravika031@users.noreply.github.com>
2023-11-03 06:35:02 +0800

    Break & continue dalam Perulangan

---
+++ Day26.java
@@ -0,0 +1,14 @@
+package day26;
+public class Day26 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if (i == 7) {
+                break;
+            }
+            if (i == 3) {
+                continue;
+            }
+            System.out.println(i);
+    }
+  }  
+}
