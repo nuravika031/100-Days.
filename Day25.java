7f741db9c45bfcad5a63e3585b91f62b038dc48f
Nur Avika <146628831+nuravika031@users.noreply.github.com>
2023-11-02 19:07:35 +0800

    Perulangan Infinite

---
+++ Day25.java
@@ -0,0 +1,11 @@
+package day25;
+public class Day25 {
+    public static void main(String[] args) {
+        int i = 1;
+        // Perulangan infinite
+        while (true) {
+            System.out.println(i);
+            i++;
+    }
+  }
+}
