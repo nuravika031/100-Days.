1dc229e1adb0c56f9df4a877c628ef01366b251e
Nur Avika <146628831+nuravika031@users.noreply.github.com>
2023-11-22 18:45:44 +0800

    Hitung Total dan Rata-rata Nilai dgn perulangan

---
+++ Day45.java
@@ -0,0 +1,26 @@
+package day45;
+
+import java.util.Scanner;
+
+public class Day45 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan jumlah nilai yang akan dihitung: ");
+        int jumlahNilai = scanner.nextInt();
+        int totalNilai = 0;
+
+        for (int i = 1; i <= jumlahNilai; i++) {
+            System.out.print("Masukkan nilai ke-" + i + ": ");
+            int nilai = scanner.nextInt();
+            totalNilai += nilai;
+        }
+        double rataRata = (double) totalNilai / jumlahNilai;
+
+        System.out.println("Total nilai: " + totalNilai);
+        System.out.println("Rata-rata nilai: " + rataRata);
+
+        scanner.close();
+    }
+}
