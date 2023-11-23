bc0e66097c76bd4988b39949a4c203b5d72f6b3e
Nur Avika <146628831+nuravika031@users.noreply.github.com>
2023-11-23 21:33:46 +0800

    Deklarasi Array bertipe primitif

---
+++ Day46.java
@@ -0,0 +1,13 @@
+package day46;
+public class Day46 {
+    public static void main(String[] args) {
+        int[] angka = {1, 2, 3, 4, 5};
+        System.out.println("Elemen array angka pada indeks 0 : " + angka[0]);
+
+        boolean[] status = new boolean[3];
+        status[0] = true;
+        status[2] = false;
+        System.out.println("Elemen array status pada indeks 2 : " + status[2]);
+    }
+    
+}
