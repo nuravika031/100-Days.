54027b2dadae04cd2df9a6e688e36559c90b9f1f
Nur Avika <146628831+nuravika031@users.noreply.github.com>
2023-11-24 22:47:00 +0800

    Deklarasi Array bertipe non-primitif

---
+++ Day47.java
@@ -0,0 +1,20 @@
+package day47;
+
+public class Day47 {
+
+    public static void main(String[] args) {
+        String[] namaArray = new String[5];
+
+        namaArray[0] = "Ika";
+        namaArray[1] = "Ria";
+        namaArray[2] = "Rifqah";
+        namaArray[3] = "Greis";
+        namaArray[4] = "Gandi";
+
+        System.out.println("Nama 1: " + namaArray[0]);
+        System.out.println("Nama 2: " + namaArray[1]);
+        System.out.println("Nama 3: " + namaArray[2]);
+        System.out.println("Nama 4: " + namaArray[3]);
+        System.out.println("Nama 5: " + namaArray[4]);
+    }
+}
