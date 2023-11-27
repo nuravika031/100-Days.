514d9e520da3714ce6da5961910f13c6801d2e30
Nur Avika <146628831+nuravika031@users.noreply.github.com>
2023-11-27 20:16:22 +0800

    Menampilkan semua elemen array dgn class Arrays

---
+++ Day50.java
@@ -0,0 +1,15 @@
+package day50;
+
+import java.util.Arrays;
+
+public class Day50 {
+    public static void main(String[] args) {
+        //Penggunaan class Arrays untuk menampilkan semua elemen array
+        int[] angka = {1, 2, 3, 4, 5};
+        System.out.println("Elemen array angka : " + Arrays.toString(angka));
+
+        String[] nama = {"Ika", "Greis", "Ria", "Rifqah"};
+        System.out.println("Elemen array nama : " + Arrays.toString(nama));
+    }
+    
+}
