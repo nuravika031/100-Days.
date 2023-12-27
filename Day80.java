4a3cb81d881d65034530413ce4df9985d36e17e2
Nur Avika <146628831+nuravika031@users.noreply.github.com>
2023-12-27 22:14:16 +0800

    Mencetak elemen2 pertama dlm sebuah array 2D

---
+++ Day80.java
@@ -0,0 +1,15 @@
+package day80;
+
+public class Day80 {
+
+    public static void main(String[] args) {
+        int[][] angka = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        for (int i = 0; i < angka.length; i++) {
+            System.out.println(angka[i][0]);
+        }
+    }
+}
