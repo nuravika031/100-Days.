e5d21b266d5b9919826a0167c88d64a8969a6750
Nur Avika <146628831+nuravika031@users.noreply.github.com>
2024-01-01 20:19:07 +0800

    Memanggil/Eksekusi Fungsi (Method)

--- Day85.java
+++ Day85.java
@@ -3,7 +3,7 @@ package day85;
 public class Day85 {
 
     static void hasil() {
-        System.out.println("Hasil dari 8 * 6 + 10 / 2 adalah : " + 8 * 6 + 10 / 2);
+        System.out.println("Hasil operasi matematika dari 8 * 6 + 10 / 2 adalah : " + 8 * 6 + 10 / 2);
     }
 
     public static void main(String[] args) {
