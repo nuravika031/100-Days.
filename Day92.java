982a90a971b154a80df45c3802e0fe18c67ea703
Nur Avika <146628831+nuravika031@users.noreply.github.com>
2024-01-08 16:41:37 +0800

    Penggunaan String charAt( )

---
+++ Day92.java
@@ -0,0 +1,14 @@
+package day92;
+
+public class Day92 {
+
+    public static void main(String[] args) {
+        String kata = "Nur Avika";
+
+        char karakter1 = kata.charAt(0);
+        char karakter2 = kata.charAt(4);
+
+        System.out.println("Karakter pada index 0: " + karakter1);
+        System.out.println("Karakter pada index 6: " + karakter2);
+    }
+}
